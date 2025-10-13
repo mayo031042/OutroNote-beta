1617612122bd0db027c8676c3a92df3c516e0574
mayo <kurumelon1219@gmail.com>
2025-10-13 16:53:32 +0900

    add test system

--- test/system/site_layouts_test.rb
+++ test/system/site_layouts_test.rb
@@ -1,9 +1,35 @@
 require "application_system_test_case"
 
 class SiteLayoutsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit site_layouts_url
-  #
-  #   assert_selector "h1", text: "SiteLayout"
+  setup do
+    @user = users(:one) # users.yml に書かれている :one というユーザー
+  end
+
+  test "testできる" do
+    assert true
+  end
+
+  test "ログインしていない時、ヘッダーにログインと新規登録リンクが表示される" do
+    visit root_url # トップページにアクセス
+
+    # "sign up"というテキストを持つ<a>タグがあることを確認
+    assert_selector "a", text: "sign up"
+    assert_selector "a", text: "sign in"
+    # # "ログアウト"というテキストを持つ<a>タグがないことを確認
+    assert_no_selector "a", text: "sign out"
+  end
+
+  # test "ログインしている時、ヘッダーにログアウトリンクが表示される" do
+  #   sign_in @user # Deviseヘルパーを使って、テスト内でログインさせます
+
+  #   visit root_url # トップページにアクセス
+
+  #   # "ログアウト"というテキストを持つ<a>タグがあることを確認
+  #   assert_selector "a", text: "sign out"
+  #   assert_no_selector "a", text: "新規登録"
+  #   assert_no_selector "a", text: "ログイン"
+
+  #   # ログインしているユーザーのメールアドレスが表示されていることを確認
+  #   assert_text @user.email
   # end
 end
