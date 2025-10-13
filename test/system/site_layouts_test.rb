73c89f0031efc15d574dc863efe88fb0c1d7fa16
mayo <kurumelon1219@gmail.com>
2025-10-13 16:24:05 +0900

    g system_test site_layout

---
+++ test/system/site_layouts_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class SiteLayoutsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit site_layouts_url
+  #
+  #   assert_selector "h1", text: "SiteLayout"
+  # end
+end
